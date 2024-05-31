05a1281338b829b49d8318c6c5bbe93fc14b6ffe
Michael Galo <michael@michaelthinks.co>
2024-05-31 11:01:50 -0500

    added notes to new animation

--- animation.js
+++ animation.js
@@ -1,3 +1,7 @@
+//The Intersection Observer API provides a way to asynchronously observe changes in the intersection of a target element with an ancestor element or with a top-level document's viewport.
+
+// We are targeting the viewport (aka scrolling) and adding or removing classes. The hidden elements are stored in the entries(array)
+
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
@@ -8,5 +12,6 @@ const observer = new IntersectionObserver((entries) => {
   });
 });
 
+// all of this is simple rendering to the DOM with a little extra flavor for the observer variable
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
